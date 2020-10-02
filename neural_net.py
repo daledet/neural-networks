94246c22beead72ea36fb0597fd04f8864f0b846
Dwight Ledet <dwightledet@gmail.com>
2020-10-02 23:13:15 +0200

    neural net

---
+++ neural_net.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+class NeuralNetwork():
+
+    def __init__(self):
+        np.random.seed(1)
+
+        self.synoptic_weights = 2 * np.random.random((3, 1)) - 1
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+    def train(self, training_inputs, training_outputs, training_iterations):
+
+        for iteration in range(training_iterations):
+
+            output = self.think(training_inputs)
+            error = training_outputs - output
+            adjustments = np.dot(training_inputs.T, error *
+                                 self.sigmoid_derivative(output))
+            self.synoptic_weights += adjustments
+
+    def think(self, inputs):
+        inputs = inputs.astype(float)
+        output = self.sigmoid(np.dot(inputs, self.synoptic_weights))
+
+        return output
+
+
+if __name__ == "__main__":
+
+    neural_network = NeuralNetwork()
+
+    print('Random Synaptic Weights: ')
+    print(neural_network.synoptic_weights)
+
+    training_inputs = np.array([[0, 0, 1],
+                                [1, 1, 1],
+                                [1, 0, 1],
+                                [0, 1, 1]])
+    training_outputs = np.array([[0, 1, 1, 0]]).T
+
+    neural_network.train(training_inputs, training_outputs, 10000)
+
+    print('Synaptic Weights After Training: ')
+    print(neural_network.synoptic_weights)
+
+    A = str(input("Input 1: "))
+    B = str(input("Input 2: "))
+    C = str(input("Input 3: "))
+
+    print("New Situation: input_data = ", A, B, C)
+    print("Output Data: ")
+    print(neural_network.think(np.array([A, B, C])))
