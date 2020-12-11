37333513cd6e1f1dc5aa21874f96149aecf5eaed
Dwight Ledet <dwightledet@gmail.com>
2020-12-11 02:42:53 +0100

    rebuild perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,37 +1,47 @@
 import numpy as np
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+class Perceptron(object):
 
+    """Perceptron Classifier
 
-def sigmoid_derivative(x):
-    return x * (1 - x)
+    Parameters
+    ----------
+    eta : float
+        learning rate (between 0.0 and 1.0)
+    n_iter : int
+        Passes over the training dataset
+    random_state : int
+        Random number generator seed for random weight initialization
 
+    Attributes
+    ----------
+    w_ : 1d-array
+        Weights after fiting
+    errors_ : list
+        Number of misclassifications (updates) in each epoch
 
-training_inputs = np.array([[0, 0, 1],
-                            [1, 1, 1],
-                            [1, 0, 1],
-                            [0, 1, 1]])
-training_outputs = np.array([[0, 1, 1, 0]]).T
+    """
 
-np.random.seed(1)
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
 
-synaptic_weights = 2 * np.random.random((3, 1)) - 1
-print('Random starting synaptic weights: ')
-print(synaptic_weights)
+    def fit(self, X, y):
+        """ Fit training data
 
-for iteration in range(20000):
-    input_layer = training_inputs
-    outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+            Training vectors where n_examples is the number of examples 
+            and n_features is the number of features
+        y : array-like, shape = [n_examples]
+            Target values
+        Returns
+        -------
+        self : object
 
-    error = training_outputs - outputs
-    adjustments = error * sigmoid_derivative(outputs)
-
-    synaptic_weights += np.dot(input_layer.T, adjustments)
-
-print('Synaptic Weights after training: ')
-print(synaptic_weights)
-
-print('Outputs after training ')
-print(outputs)
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
